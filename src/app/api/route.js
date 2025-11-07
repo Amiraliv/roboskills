c5a22b5c2ba5f61b7a5e5f91a58796fc014a4202
Amiraliv <amiralismath@gmail.com>
2025-11-08 00:46:16 +0330

    added prisma database

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,5 +3,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  console.log(process.env.DATABASE_URL);
+
   return NextResponse.json({ uptime: process.uptime() });
 }
