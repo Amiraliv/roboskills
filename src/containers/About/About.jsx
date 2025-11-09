e361d47b38f0dc911e6365ea0744a941857478a0
Amiraliv <amiralismath@gmail.com>
2025-11-09 11:52:28 +0330

    add about for roboskills

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,11 +1,56 @@
+import "./About.scss";
+import Image from "next/image";
 import { LayoutData } from "../../components";
 
 const About = () => {
   return (
     <section id="competition">
       <LayoutData>
-        <h2>RoboSkills درباره مسابقات</h2>
-        <nav className=""></nav>
+        <h2 className="competition__title">
+          <span>درباره مسابقات</span>
+          <strong>RoboSkills</strong>
+        </h2>
+
+        <nav className="competitionIndexData">
+          <div className="competition__banner">
+            <Image
+              src={"/assets/img/Logo-main.png"}
+              width={340}
+              height={250}
+              alt="roboskills logo"
+              priority
+            />
+          </div>
+
+          <div className="competition__data">
+            <p>
+              رویداد <strong>RoboSkills</strong> به‌عنوان بزرگ‌ترین رویداد
+              فناوری جنوب شرق کشور، با هدف توسعه مهارت‌های فناورانه، ترویج
+              خلاقیت و ایجاد پیوند میان دانشگاه، صنعت و آموزش در شهر کرمان
+              برگزار خواهد شد. این رویداد فرصتی است برای شکوفایی استعدادهای
+              جوان، ارتقای مهارت‌های آینده و خلق راه‌حل‌های نوآورانه برای
+              چالش‌های واقعی جامعه.
+            </p>
+          </div>
+
+          <div className="competition__timeline">
+            <div className="competition__timeline_header">
+              <h3>⏰ زمان‌بندی برگزاری مسابقات</h3>
+            </div>
+            <div className="competition__timeline_data">
+              <ul>
+                <li>
+                  <span>بهمن 1404</span>
+                  <span>مرحله مقدماتی</span>
+                </li>
+                <li>
+                  <span>اردیبهشت 1405</span>
+                  <span>مرحله نهایی</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </LayoutData>
     </section>
   );
